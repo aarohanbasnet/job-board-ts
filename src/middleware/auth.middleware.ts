8ac70acd5e67b5d229db378a7ab78d5fb775f0b0
aarohanbasnet <letsmailaarohan@gmail.com>
2026-02-07 23:24:08 +0545

    feat(auth): add authorizeRoles middleware for multi-role access control

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -50,3 +50,24 @@ export const isLoggedIn = function(req : customRequest, res: Response, next : Ne
         };
     }
 };
+
+export const authorizeRoles = function (...allowedRoles : string[]){
+ return (req : customRequest, res : Response, next : NextFunction) =>{ 
+    //first isLoggedin middleware should run else req.user will be undefined and we cannot access the properties
+    if(!req.user){
+        return res.status(401).json({
+            success : false,
+            message : "User not authenticated"
+        });
+    }
+
+    if(!allowedRoles.includes(req.user.role)){
+        return res.status(403).json({
+            success : false,
+            message : `Role ${req.user.role} is not authorized to access this resource`
+        });
+    }
+
+    next();
+ }
+}
